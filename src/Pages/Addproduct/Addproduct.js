eeeefde0ef4296d970188af9bab3211c695fbbf2
tauhid880 <tauhid880@gmail.com>
2022-11-29 02:05:02 +0600

    Add product page added

--- src/Pages/Addproduct/Addproduct.js
+++ src/Pages/Addproduct/Addproduct.js
@@ -2,8 +2,124 @@ import React from "react";
 
 const Addproduct = () => {
   return (
-    <div>
-      <h1>This is Add product page</h1>
+    <div className="lg:h-[800px] h-auto lg:p-10 p-5  ">
+      <div className="shadow-xl bg-zinc-300 text-black p-7 ">
+        <h1 className="text-xl font-semibold  rounded-md text-center my-5">
+          Add your product
+        </h1>
+        <form>
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3">
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text">Product name</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered  "
+                placeholder="product name"
+              />
+            </div>
+            <div className="form-control w-full ">
+              <label className="label ">
+                <span className="label-text">Product original price</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered  "
+                placeholder="Original Price"
+              />
+            </div>
+            <div className="form-control w-full ">
+              <label className="label ">
+                <span className="label-text">Product resell price</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered  "
+                placeholder="Resell Price"
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Product condition</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                placeholder="condition"
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Phone Number</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                placeholder="Your Phone"
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Location</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                placeholder="Your Location"
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Product category</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                placeholder="product category"
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Year of purchase</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                placeholder="Purchase Year"
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Year of use</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                placeholder="Year of use"
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Product Image</span>
+              </label>
+              <input
+                type="text"
+                className="input input-bordered w-full"
+                placeholder="Image"
+              />
+            </div>
+          </div>
+
+          <div>
+            <input
+              className="btn btn-success text-gray-700 w-full my-5 "
+              value="Add Product"
+              type="submit"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
