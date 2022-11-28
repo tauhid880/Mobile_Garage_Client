93a5b07a3161a79ff520cc23f92c2100dde9b994
tauhid880 <tauhid880@gmail.com>
2022-11-29 01:16:49 +0600

    all sellers added

--- src/Pages/Allseller/Allseller.js
+++ src/Pages/Allseller/Allseller.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Allseller = () => {
+  const [sellers, setSellers] = useState([]);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/users/Seller`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSellers(data);
+      });
+  }, []);
   return (
     <div>
-      <h1>This is all seller Pages</h1>
+      <h1 className="text-2xl text-center my-5 font-bold">All Sellers</h1>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sellers.map((seller, i) => (
+              <tr key={seller._id} className="active">
+                <th>{i + 1}</th>
+                <td>{seller.name}</td>
+                <td>{seller.email}</td>
+                <td>
+                  <button className="btn btn-sm btn-secondary">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
